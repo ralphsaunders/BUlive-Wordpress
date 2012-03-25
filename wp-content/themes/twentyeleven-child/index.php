2c684db121f08ec3179d29f48901e267ec42927e
Ralph Saunders <e@ralphsaunders.co.uk>
2012-03-25 23:01:34 +0100

    Fixed Layout Category Thumbs

--- wp-content/themes/twentyeleven-child/index.php
+++ wp-content/themes/twentyeleven-child/index.php
@@ -34,33 +34,7 @@ get_header(); ?>
                 <img id="play" src="<?php echo bloginfo( 'stylesheet_directory' ); ?>/img/play_thumb.png" />
             </div>
 
-            <div class="post-thumbs" id="content" role="main">
-
-            <?php if ( have_posts() ) : ?>
-
-                <?php /* Start the Loop */ ?>
-                <?php while ( have_posts() ) : the_post(); ?>
-
-                    <?php get_template_part( 'content', 'thumbs' ); ?>
-
-                <?php endwhile; ?>
-
-                <?php twentyeleven_content_nav( 'nav-below' ); ?>
-
-            <?php else : ?>
-
-                <article id="post-0" class="post no-results not-found">
-                    <header class="entry-header">
-                        <h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
-                    </header><!-- .entry-header -->
-
-                    <div class="entry-content">
-                        <p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
-                        <?php get_search_form(); ?>
-                    </div><!-- .entry-content -->
-                </article><!-- #post-0 -->
-
-            <?php endif; ?>
+            <?php get_template_part( 'content', 'thumbs' ); ?>
 
             </div><!-- #content -->
         </div><!-- #primary -->
