ffd82e311489837f2b959f076f30e9a12131a9cc
Matt Cheetham <m4ttcheetham@gmail.com>
2012-03-25 22:54:50 +0100

    Styled view post page

--- wp-content/themes/twentyeleven-child/content-page.php
+++ wp-content/themes/twentyeleven-child/content-page.php
@@ -9,12 +9,13 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<h1>BUHHHH</h1>
 	<header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php the_content(); ?>
+		<?php // the_content(); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
 	</div><!-- .entry-content -->
 	<footer class="entry-meta">
---
+++ wp-content/themes/twentyeleven-child/content-single.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying content in the single.php template
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+?>
+
+<div id="PostWrapper">
+
+<h1 class="entry-title"><?php the_title(); ?></h1><br>
+
+	<p><?php the_content(); ?></p>
+
+</div>
